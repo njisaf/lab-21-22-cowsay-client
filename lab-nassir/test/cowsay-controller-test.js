60fd602158b050c8684356aca038e05191ef5539
Nassir Isaf <njisaf@gmail.com>
2016-10-19 14:24:24 -0700

    Finally got karma running and a test working

---
+++ lab-nassir/test/cowsay-controller-test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// const cowsay = require('cowsay-browser');
+// const angular = require('angular');
+
+describe('test test', function() {
+  it('should be true', () => {
+    expect(true).toBe(true);
+  });
+});
